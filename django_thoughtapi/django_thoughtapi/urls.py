bae5178cf0ef9d51cab2780368787d530f54184c
Norton Pengra <npengra317@gmail.com>
2016-04-28 16:20:37 -0700

    Included media server for debugging

--- django_thoughtapi/django_thoughtapi/urls.py
+++ django_thoughtapi/django_thoughtapi/urls.py
@@ -15,9 +15,16 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from imagersite import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^accounts/', include('registration.backends.hmac.urls')),
     url(r'^images/', include('images_api.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
