33753c89e3d336addecc843b86c021856cbef3a0
Norton Pengra <npengra317@gmail.com>
2016-04-29 12:44:18 -0700

    suffix_patterns included on all api views

--- django_thoughtapi/images_api/apis.py
+++ django_thoughtapi/images_api/apis.py
@@ -1,6 +1,9 @@
+from rest_framework.urlpatterns import format_suffix_patterns
 from django.conf.urls import url
 from . import api_views
 
 urlpatterns = [
     url(r'$', api_views.root, name='root'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
