f32765e520fa9f997116abfb5eb27577b6c8a8ed
Norton Pengra <npengra317@gmail.com>
2016-04-29 12:46:32 -0700

    Routes included for album and photo api

--- django_thoughtapi/images_api/apis.py
+++ django_thoughtapi/images_api/apis.py
@@ -4,6 +4,8 @@ from . import api_views
 
 urlpatterns = [
     url(r'$', api_views.root, name='root'),
+    url(r'photo/$', api_views.photo, name='photo'),
+    url(r'album/$', api_views.album, name='album'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
