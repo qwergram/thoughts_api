a912db4c8dfc9197063d42ebae2c2d4bc5bf6b55
Norton Pengra <npengra317@gmail.com>
2016-04-28 21:23:01 -0700

    Updated name to fit with namespace

--- django_thoughtapi/images_api/api_views.py
+++ django_thoughtapi/images_api/api_views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.reverse import reverse
 
 @api_view(['GET'])
-def api_root(request, format=None):
+def root(request, format=None):
     return Response({
         'users': reverse('user-list', request=request, format=format),
         'snippets': reverse('snippet-list', request=request, format=format)
