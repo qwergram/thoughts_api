f38a78f81316be5150fec25afc28704515d7a1b6
Norton Pengra <npengra317@gmail.com>
2016-04-29 13:00:57 -0700

    New photo view for api

--- django_thoughtapi/images_api/api_views.py
+++ django_thoughtapi/images_api/api_views.py
@@ -1,8 +1,9 @@
 from .models import Photo, Album, PUBLIC
-from .serializers import PhotoSerializer
+from .serializers import PhotoSerializer, AlbumSerializer
 from django.http import HttpResponse
 from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
+import json
 from django.views.decorators.csrf import csrf_exempt
 
 
@@ -11,7 +12,7 @@ class JSONResponse(HttpResponse):
     An HttpResponse that renders its content into JSON.
     """
     def __init__(self, data, **kwargs):
-        content = JSONRenderer().render(data)
+        content = json.dumps(data, indent=2)
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
 
@@ -21,4 +22,21 @@ def root(request):
     if request.method == 'GET':
         photos = Photo.objects.filter(published=PUBLIC).order_by("-date_uploaded")
         serializer = PhotoSerializer(photos, many=True)
+        return JSONResponse(serializer.data)
+
+
+@csrf_exempt
+def photo(request):
+    if request.method == 'GET':
+        photos = Photo.objects.filter(published=PUBLIC).order_by("-date_uploaded")
+        serializer = PhotoSerializer(photos, many=True)
+        return JSONResponse(serializer.data)
+
+
+
+@csrf_exempt
+def album(request):
+    if request.method == 'GET':
+        albums = Album.objects.filter(published=PUBLIC).order_by("-date_uploaded")
+        serializer = AlbumSerializer(albums, many=True)
         return JSONResponse(serializer.data)
