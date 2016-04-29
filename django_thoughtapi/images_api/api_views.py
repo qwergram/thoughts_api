82ae9e78cb6cfae45a7784113db9bb25299b0c0e
Norton Pengra <npengra317@gmail.com>
2016-04-29 12:30:31 -0700

    New API view for library view

--- django_thoughtapi/images_api/api_views.py
+++ django_thoughtapi/images_api/api_views.py
@@ -1,6 +1,8 @@
-from .models import Photo, Album
+from .models import Photo, Album, PUBLIC
+from .serializers import PhotoSerializer
 from django.http import HttpResponse
 from rest_framework.renderers import JSONRenderer
+from django.views.decorators.csrf import csrf_exempt
 
 class JSONResponse(HttpResponse):
     """
@@ -10,3 +12,12 @@ class JSONResponse(HttpResponse):
         content = JSONRenderer().render(data)
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
+
+@csrf_exempt
+def root(request):
+    if request.method == 'GET':
+        photos = Photo.objects.filter(published=PUBLIC).order_by("-date_uploaded")
+        serializer = PhotoSerializer(photos, many=True)
+        return JSONResponse(serializer.data)
+
+    
