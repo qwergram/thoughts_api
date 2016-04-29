b16fb33ca4a722bc0831e4f22b65ef31a0fb2528
Norton Pengra <npengra317@gmail.com>
2016-04-28 17:55:40 -0700

    Edit Profile form created

--- django_thoughtapi/images_api/forms.py
+++ django_thoughtapi/images_api/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.conf import settings
 from .models import Photo, Album
 
 
@@ -16,3 +17,9 @@ class NewAlbum(forms.ModelForm):
     def __init__(self, profile_=None, *args, **kwargs):
         super(NewAlbum, self).__init__(*args, **kwargs)
         self.fields['photos'].queryset = self.fields['photos'].queryset.filter(owner=profile_)
+
+
+class EditProfile(forms.ModelForm):
+    class Meta:
+        model = settings.AUTH_USER_MODEL
+        fields = ['first_name', 'last_name', 'email']
