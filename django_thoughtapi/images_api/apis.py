0d1b97ac24616965fb6b4e30b219436f545aea20
Norton Pengra <npengra317@gmail.com>
2016-04-29 13:00:27 -0700

    Changed order of routes

--- django_thoughtapi/images_api/apis.py
+++ django_thoughtapi/images_api/apis.py
@@ -3,9 +3,9 @@ from django.conf.urls import url
 from . import api_views
 
 urlpatterns = [
-    url(r'$', api_views.root, name='root'),
-    url(r'photo/$', api_views.photo, name='photo'),
     url(r'album/$', api_views.album, name='album'),
+    url(r'photo/$', api_views.photo, name='photo'),
+    url(r'$', api_views.root, name='root'),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
