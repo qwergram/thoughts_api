1f498f8a0975a1bba38b4794d7a0030d83926caa
Norton Pengra <npengra317@gmail.com>
2016-04-29 12:34:30 -0700

    Cleaned up some code

--- django_thoughtapi/images_api/api_views.py
+++ django_thoughtapi/images_api/api_views.py
@@ -2,7 +2,9 @@ from .models import Photo, Album, PUBLIC
 from .serializers import PhotoSerializer
 from django.http import HttpResponse
 from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
 from django.views.decorators.csrf import csrf_exempt
+
 
 class JSONResponse(HttpResponse):
     """
@@ -13,11 +15,10 @@ class JSONResponse(HttpResponse):
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
 
+
 @csrf_exempt
 def root(request):
     if request.method == 'GET':
         photos = Photo.objects.filter(published=PUBLIC).order_by("-date_uploaded")
         serializer = PhotoSerializer(photos, many=True)
         return JSONResponse(serializer.data)
-
-    
